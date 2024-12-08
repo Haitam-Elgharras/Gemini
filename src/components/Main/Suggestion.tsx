b1985874c283f673978abe7d895eba2a7a9142f3
haitam-elgharras <elgharras.haitam@gmail.com>
2024-12-08 13:26:09 +0100

    update page title and enhance Suggestion component styling

--- src/components/Main/Suggestion.tsx
+++ src/components/Main/Suggestion.tsx
@@ -6,9 +6,9 @@ interface SuggestionProps {
 
 const Suggestion = ({ text, icon }: SuggestionProps) => {
   return (
-    <div className="card flex flex-col items-center py-3 bg-[#f0f4f9] rounded-xl h-[200px] justify-between hover:bg-[#dfe4ea] cursor-pointer">
-        <p className="mx-3 px-3 text-lg text-gray-600 text-[14px]">{text}</p>
-        <img src={icon} alt="icon" className="w-8 rounded-full bg-white p-1 self-end mr-2" />
+    <div className="card relative py-3 bg-[#f0f4f9] rounded-[10px] h-[200px] hover:bg-[#dfe4ea] cursor-pointer">
+        <p className="mx-3 px-3 text-lg text-gray-600 text-[17px]">{text}</p>
+        <img src={icon} alt="icon" className="w-8 rounded-full bg-white p-1 self-end absolute right-3 bottom-3" />
     </div>
   )
 }
